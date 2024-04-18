eb60106d9b7cc3249bee6e5c89ebf784f851ae10
Md. Mehedi Hasan <108575130+mehedidevs@users.noreply.github.com>
2024-04-18 16:56:39 +0600

    mvvm added

--- app/src/main/java/com/creativeitinstitute/letsbuy/views/starter/MainActivity.kt
+++ app/src/main/java/com/creativeitinstitute/letsbuy/views/starter/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.creativeitinstitute.letsbuy
+package com.creativeitinstitute.letsbuy.views.starter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.creativeitinstitute.letsbuy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
